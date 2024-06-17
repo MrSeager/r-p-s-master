e6b4e83d2b29ff4888c116201e29fbc603e316cc
MrSeager <seager1993@gmail.com>
2024-06-17 11:50:52 +0300

    6/17/2024

--- src/MainGame.js
+++ src/MainGame.js
@@ -22,12 +22,17 @@ const MainGame = () => {
     const [showGameStep2, setShowGameStep2] = useState(false);
     const [userPick, setUserPick] = useState('');
     const [housePick, setHousePick] = useState('');
-    
-    const [result, setResult] = useState('');
+    const [playerScore, setPlayerScore] = useState(JSON.parse(localStorage.getItem('playerScore')) || 0);
+    const [houseScore, setHouseScore] = useState(JSON.parse(localStorage.getItem('houseScore')) || 0);
 
     const toggleRulesPage = () => {
         setShowRules(!showRules);
     };
+
+    useEffect(() => {
+        localStorage.setItem('playerScore', JSON.stringify(playerScore));
+        localStorage.setItem('houseScore', JSON.stringify(houseScore));
+      }, [playerScore, houseScore]);
 
     AOS.init();
 
@@ -36,8 +41,8 @@ const MainGame = () => {
             <Container data-aos="fade-down" className='border rounded d-flex flex-row justify-content-between w-75 mt-5 py-2'>
                 <h1 className='text-uppercase display-5 text-white p-0 m-0'>Rock<br /> Paper<br /> Scissors</h1>
                 <Container className='w-25 bg-white rounded m-3 d-flex flex-column align-items-center justify-content-center'>
-                    <h2 className='text-uppercase h4'>Score</h2>
-                    <h3 className='display-3'>{0}</h3>
+                    <h2 className='text-uppercase cs-fc h4'>Score</h2>
+                    <h3 className='display-3 cs-fc-2'>{playerScore} x {houseScore}</h3>
                 </Container>
             </Container>
             {showGameStep1 && <GameStep1 setShowGameStep1={setShowGameStep1} 
@@ -46,6 +51,8 @@ const MainGame = () => {
                                          setHousePick={setHousePick} />}
             {showGameStep2 && <GameStep2 setShowGameStep2={setShowGameStep2} 
                                          setShowGameStep1={setShowGameStep1}
+                                         setPlayerScore={setPlayerScore}
+                                         setHouseScore={setHouseScore}
                                          userPick={userPick}
                                          housePick={housePick} />}
             <Button onClick={toggleRulesPage} className='cs-btn text-uppercase px-4 py-2'>Rules</Button> 
@@ -56,34 +63,41 @@ const MainGame = () => {
 
 const GameStep1 = ({setUserPick, setShowGameStep1, setShowGameStep2, setHousePick}) => {
     const options = ['paper', 'scissors', 'rock'];
-    
+
     const randomizePick = () => {
-        const randomIndex = Math.floor(Math.random() * options.length); // Generates a random index between 0 and 2
+        const randomIndex = Math.floor(Math.random() * options.length);
         return options[randomIndex];
+    };
+
+    const handlePlayerPick = (pick) => {
+        setUserPick(pick);
+        setShowGameStep1(false); 
+        setShowGameStep2(true);
+        setHousePick(randomizePick());
     };
     
     return (
         <Container className='mt-5 cs-w'>
             <Col className='mx-auto'>
-                <Row data-aos="fade-right" className='justify-content-between'>
-                    <Button className='cs-btn-def cs-btn-paper' 
-                            onClick={() => (setUserPick('paper'), 
-                                           setShowGameStep1(false), 
-                                           setShowGameStep2(true),
-                                           setHousePick(randomizePick()))}><Image src={ImgPaper} alt='paper' /></Button>
-                    <Button className='cs-btn-def cs-btn-scissors' onClick={() => (setUserPick('scissors'), setShowGameStep1(false), setShowGameStep2(true))}><Image src={ImgScissors} alt='scissors' /></Button>
+                <Row className='justify-content-between'>
+                    <Button data-aos="fade-right" className='rounded-circle cs-btn-def cs-btn-paper' 
+                            onClick={() => handlePlayerPick(options[0])}><Image src={ImgPaper} alt='paper' /></Button>
+                    <Button data-aos="fade-left" className='rounded-circle cs-btn-def cs-btn-scissors' 
+                            onClick={() => handlePlayerPick(options[1])}><Image src={ImgScissors} alt='scissors' /></Button>
                 </Row>
-                <Row data-aos="fade-left" className='mt-4 justify-content-center'>
-                    <Button className='cs-btn-def cs-btn-rock' onClick={() => (setUserPick('rock'), setShowGameStep1(false), setShowGameStep2(true))}><Image src={ImgRock} alt='rock' /></Button>
+                <Row data-aos="fade-down" className='mt-4 justify-content-center'>
+                    <Button className='rounded-circle cs-btn-def cs-btn-rock' 
+                            onClick={() => handlePlayerPick(options[2])}><Image src={ImgRock} alt='rock' /></Button>
                 </Row>
             </Col>
         </Container>
     );
 };
 
-const GameStep2 = ({ userPick, housePick, result, setShowGameStep1, setShowGameStep2 }) => {
+const GameStep2 = ({ userPick, housePick, setShowGameStep1, setShowGameStep2, setPlayerScore, setHouseScore }) => {
     const [userPickImg, setUserPickImg] = useState();
     const [housePickImg, setHousePickImg] = useState();
+    const [result, setResult] = useState('');
 
     const handlePick = () => {
         if (userPick === 'paper') {
@@ -105,25 +119,46 @@ const GameStep2 = ({ userPick, housePick, result, setShowGameStep1, setShowGameS
         }
     };
 
+    const handleResult = () => {
+        if (userPick === housePick) {
+            setResult('Drow')
+        } else if ((userPick === 'paper' && housePick !== 'scissors') ||
+                   (userPick === 'scissors' && housePick !== 'rock') ||
+                   (userPick === 'rock' && housePick !== 'paper')) {
+            setResult('You win');
+            setPlayerScore(prevScore => prevScore + 1);
+        } else {
+            setResult('You lose');
+            setHouseScore(prevScore => prevScore + 1);
+        };
+    };
+
     useEffect(() => {
         handlePick();
         handleHousePick();
-      }, [userPick]);
+        handleResult();
+    }, [userPick]);
+
+    const handlePlayAgain = () => {
+        setShowGameStep1(true); 
+        setShowGameStep2(false);
+    };
 
     return (
         <Container className='mt-5 w-75'>
             <Row className='text-white text-uppercase'>
-                <Col className='d-flex flex-column align-items-center'>
+                <Col data-aos="fade-right" className='d-flex flex-column align-items-center'>
                     <h4 className='text-center'>You picked</h4>
-                    <Button disabled className={`cs-btn-def cs-btn-${userPick}`}><Image src={userPickImg} alt='user pick' /></Button>
+                    <Button disabled className={`cs-btn-def rounded-circle cs-btn-${userPick} ${result === 'You win' && 'cs-win'}`}><Image src={userPickImg} alt='user pick' /></Button>
                 </Col>
-                <Col className='d-flex flex-column align-items-center justify-content-center'>
-                    <h4>{result}</h4>
-                    <Button className='cs-btn-2 py-3 px-4 text-uppercase' onClick={() => (setShowGameStep1(true), setShowGameStep2(false))}>Play again</Button>
+                <Col data-aos="fade-up" className='d-flex flex-column align-items-center justify-content-center'>
+                    <h4 className='display-4'>{result}</h4>
+                    <Button className='cs-btn-2 py-3 px-4 text-uppercase' 
+                            onClick={handlePlayAgain}>Play again</Button>
                 </Col>
-                <Col className='d-flex flex-column align-items-center'>
+                <Col data-aos="fade-left" className='d-flex flex-column align-items-center'>
                     <h4>The house picked</h4>
-                    <Button disabled className={`cs-btn-def cs-btn-${housePick}`}><Image src={housePickImg} alt='house pick' /></Button>
+                    <Button disabled className={`cs-btn-def rounded-circle cs-btn-${housePick} ${result === 'You lose' && 'cs-win'}`}><Image src={housePickImg} alt='house pick' /></Button>
                 </Col>
             </Row>
         </Container>
